3e4a29081acdc2bd7412ef5e6965c2d28fbc03e5
Joseph Utecht <thenmal@gmail.com>
2016-04-14 13:44:08 -0500

    moved to new definition pipe

--- app/definition.pipe.ts
+++ app/definition.pipe.ts
@@ -10,6 +10,7 @@ import {Definition} from './question';
 export class DefinitionPipe implements PipeTransform {
     public definitions: Definition[];
     public errored: boolean = false;
+    private _regex: RegExp = /<([^>]+)\|([^>]+)>/g;
     
     constructor(private _definitionService: DefinitionService){
         _definitionService.getDefinitions()
@@ -18,10 +19,21 @@ export class DefinitionPipe implements PipeTransform {
     
     }
 
+    getDefinition(word: string){
+        for(let i = 0; i < this.definitions.length; i++){
+            if(this.definitions[i].word === word){
+                return this.definitions[i].definition;
+            }
+        }
+        return "Missing definition: " + word;
+    }
+
     transform(value: string, args:string[]):any {
         if(!this.errored && this.definitions){
-            for(let i = 0; i < this.definitions.length; i++){
-                value = value.replace(this.definitions[i].word, this.wrapWord(this.definitions[i]));
+            let matches = this._regex.exec(value);
+            while(matches){
+                value = value.replace(matches[0], this.wrapWord(matches[1], this.getDefinition(matches[2])));
+                matches = this._regex.exec(value);
             }
             return value;
         } else {
@@ -30,12 +42,12 @@ export class DefinitionPipe implements PipeTransform {
     }
 
     // Wrap DIV around the word
-    wrapWord(definition: Definition){
+    wrapWord(word: string, definition: string){
         return `<span style="color:red"
                       data-container="body"
                       data-toggle="popover"
                       data-trigger="hover"
                       data-placement="top"
-                      data-content="${definition.definition}">${definition.word}</span>`;
+                      data-content="${definition}">${word}</span>`;
     }
 }
